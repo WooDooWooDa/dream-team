e8cc3fc3b6e19b12edda870c90f637a52a254ab7
YuuHappy <93843539+YuuHappy@users.noreply.github.com>
2021-11-06 20:36:43 -0400

    up

--- functions.py
+++ functions.py
@@ -21,29 +21,31 @@ y = pos[1, 0]
 z = pos[2, 0]
 
 #fonctions
-def get_module(x ,y ,z):
+def get_module(x, y, z):
     return math.sqrt(x**2 + y**2 + z**2)
 
-def get_gamma(V):
+def get_gamma():
     return 1/(math.sqrt(1.0 - (V**2)))
 
-def transform_x(x):
-    return get_gamma(V) * (x - (V*t))
+def transform_x():
+    return get_gamma() * (x - (V*t))
 
-def transform_t(t):
-    return get_gamma(V) * (t - (V * x))
+def transform_t():
+    return get_gamma() * (t - (V * x))
 
 #Variables dans S'
 xp = transform_x(x)
 yp = y
 zp = z
-tp = 
+tp = transform_t(t)
 
 
-#Champ magnétique dans S
-Etr = Q/(4 * math.pi * e0 * get_module(x, y, z)**3)
-
-for i, element in enumerate(pos):
-    pos[i] = element * Etr
+#Champ électrique dans S
+def get_champ():
+    return (Q / (4 * math.pi * e0 * get_module(x, y, z)**3)) * pos
     
-
+#Champ électrique dans S'
+def get_champprime():   
+    pos[0] *= get_gamma()
+    pos[1] *= get_gamma()
+    return (Q / (4 * math.pi * e0 * get_module(x, y, z)**3)) * pos
